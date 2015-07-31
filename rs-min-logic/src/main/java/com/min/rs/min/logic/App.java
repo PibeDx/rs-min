0e4f49223222b42bf968cf0162f27909b79ac428
José Cuentas <jcuentast@gmail.com>
2015-07-30 19:26:40 -0500

    primer commit

---
+++ rs-min-logic/src/main/java/com/min/rs/min/logic/App.java
@@ -0,0 +1,13 @@
+package com.min.rs.min.logic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
